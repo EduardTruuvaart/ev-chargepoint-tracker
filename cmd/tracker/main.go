01bd117d8ee912aaf3d40debda74cad5cb38789c
Eduard Truuvaart <eduard.truuvaart@gmail.com>
2020-10-30 15:07:55 +0000

    service is added

--- cmd/tracker/main.go
+++ cmd/tracker/main.go
@@ -1,14 +1,11 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
-	"io/ioutil"
-	"net/http"
 	"os"
 	"strconv"
 
-	"github.com/EduardTruuvaart/ev-chargepoint-tracker/domain/model"
+	"github.com/EduardTruuvaart/ev-chargepoint-tracker/service"
 )
 
 func main() {
@@ -18,44 +15,9 @@ func main() {
 		fmt.Println("APIKEY env var is not set!")
 		return
 	}
-	fmt.Println("Key: ", apiKey)
-	fmt.Println("Selected station ID: ", stationID)
-	client := &http.Client{}
 
-	requestURI := fmt.Sprintf("https://api.zap-map.com/v5/chargepoints/location/%d/status", stationID)
-	req, err := http.NewRequest("GET", requestURI, nil)
-	req.Header.Add("X-Api-Key", apiKey)
-
-	if err != nil {
-		fmt.Println("Get Error")
-	}
-
-	resp, err := client.Do(req)
-
-	if err != nil {
-		fmt.Println("Get Error")
-	}
-
-	defer resp.Body.Close()
-
-	body, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		fmt.Println("Body Error")
-	}
-
-	bodyStr := string(body)
-
-	var result map[string]interface{}
-	json.Unmarshal([]byte(bodyStr), &result)
-
-	var chargePointsData interface{} = result["resources"].(map[string]interface{})["chargepoint_location_status"].(map[string]interface{})["data"]
-	var devices interface{} = chargePointsData.(map[string]interface{})["devices"]
-	devicesArr := devices.([]interface{})
-	var statusHistory []interface{} = devicesArr[0].((map[string]interface{}))["status_history"].([]interface{})
-	var currentStatus string = statusHistory[0].(map[string]interface{})["description"].(string)
-	var stationIDJson float64 = chargePointsData.(map[string]interface{})["id"].(float64)
-
-	var station *model.Station = model.NewStation(stationIDJson, currentStatus)
+	stationService := new(service.StationService)
+	station := stationService.GetStatus(stationID, apiKey)
 	fmt.Println(station)
 }
 
