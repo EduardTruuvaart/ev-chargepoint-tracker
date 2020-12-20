ea2f01616a43eb02a1406ecfb5d184acf1dbf581
Eduard Truuvaart <eduard.truuvaart@gmail.com>
2020-12-20 15:25:34 +0000

    wip

--- cmd/tracker/main.go
+++ cmd/tracker/main.go
@@ -1,52 +1,117 @@
 package main
 
 import (
-	"fmt"
+	"context"
+	"encoding/json"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"net/url"
 	"os"
+	"strconv"
 
-	"github.com/EduardTruuvaart/ev-chargepoint-tracker/domain/model"
-	"github.com/EduardTruuvaart/ev-chargepoint-tracker/repository"
-	"github.com/EduardTruuvaart/ev-chargepoint-tracker/service"
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+
+	"github.com/EduardTruuvaart/ev-chargepoint-tracker/domain/model/bot"
 )
 
-func main() {
-
-	stationID := os.Getenv("STATIONID")
-	if len(stationID) == 0 {
-		panic("StationID is not provided")
+func parseTelegramRequest(r *http.Request) (*bot.Update, error) {
+	var update bot.Update
+	if err := json.NewDecoder(r.Body).Decode(&update); err != nil {
+		log.Printf("could not decode incoming update %s", err.Error())
+		return nil, err
 	}
-
-	apiKey := os.Getenv("APIKEY")
-	if len(apiKey) == 0 {
-		panic("APIKEY env var is not set!")
-	}
-
-	var stationService service.StationService
-	var stationRepository repository.StationRepository
-
-	savedStationStatus, err := stationRepository.FindByID(stationID)
-	if err != nil {
-		fmt.Println("Repository error occured: ", err)
-		return
-	}
-
-	var currentStationStatus *model.Station = stationService.GetStatus(stationID, apiKey)
-
-	if savedStationStatus == nil {
-		stationRepository.Save(currentStationStatus)
-		notifyStatusChanged(currentStationStatus.Status)
-		return
-	}
-
-	if currentStationStatus.Status != savedStationStatus.Status {
-		stationRepository.Save(currentStationStatus)
-		notifyStatusChanged(currentStationStatus.Status)
-		return
-	}
-
-	fmt.Println("Status unchanged: ", currentStationStatus.Status)
+	return &update, nil
 }
 
-func notifyStatusChanged(newStatus string) {
-	fmt.Println("New status: ", newStatus)
+// HandleTelegramWebHook sends a message back to the chat with a punchline starting by the message provided by the user.
+func HandleTelegramWebHook(w http.ResponseWriter, r *http.Request) {
+	// Parse incoming request
+	var update, err = parseTelegramRequest(r)
+	if err != nil {
+		log.Printf("error parsing update, %s", err.Error())
+		return
+	}
+
+	// Sanitize input
+	var sanitizedSeed = sanitize(update.Message.Text)
+
+	// Call RapLyrics to get a punchline
+	var lyric, errRapLyrics = getPunchline(sanitizedSeed)
+	if errRapLyrics != nil {
+		log.Printf("got error when calling RapLyrics API %s", errRapLyrics.Error())
+		return
+	}
+
+	// Send the punchline back to Telegram
+	var telegramResponseBody, errTelegram = sendTextToTelegramChat(update.Message.Chat.ID, lyric)
+	if errTelegram != nil {
+		log.Printf("got error %s from telegram, reponse body is %s", errTelegram.Error(), telegramResponseBody)
+	} else {
+		log.Printf("punchline %s successfuly distributed to chat id %d", lyric, update.Message.Chat.ID)
+	}
+}
+
+func sanitize(text string) string {
+	return text
+}
+
+func getPunchline(text string) (string, error) {
+	return text, nil
+}
+
+// sendTextToTelegramChat sends a text message to the Telegram chat identified by its chat Id
+func sendTextToTelegramChat(chatID int, text string) (string, error) {
+
+	log.Printf("Sending %s to chat_id: %d", text, chatID)
+	var telegramAPI string = "https://api.telegram.org/bot" + os.Getenv("TELEGRAM_BOT_TOKEN") + "/sendMessage"
+	response, err := http.PostForm(
+		telegramAPI,
+		url.Values{
+			"chat_id": {strconv.Itoa(chatID)},
+			"text":    {text},
+		})
+
+	if err != nil {
+		log.Printf("error when posting text to the chat: %s", err.Error())
+		return "", err
+	}
+	defer response.Body.Close()
+
+	var bodyBytes, errRead = ioutil.ReadAll(response.Body)
+	if errRead != nil {
+		log.Printf("error in parsing telegram answer %s", errRead.Error())
+		return "", err
+	}
+	bodyString := string(bodyBytes)
+	log.Printf("Body of Telegram Response: %s", bodyString)
+
+	return bodyString, nil
+}
+
+func handle(ctx context.Context, name events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	log.Print("Request body: ", name)
+	log.Print("context ", ctx)
+	headers := map[string]string{"Access-Control-Allow-Origin": "*", "Access-Control-Allow-Headers": "Origin, X-Requested-With, Content-Type, Accept"}
+
+	code := 200
+	// response, error := json.Marshal(MyReturn{Response: "Hello, " + name.Body})
+	// if error != nil {
+	// 	log.Println(error)
+	// 	response = []byte("Internal Server Error")
+	// 	code = 500
+	// }
+
+	return events.APIGatewayProxyResponse{
+		StatusCode:        code,
+		Headers:           headers,
+		MultiValueHeaders: http.Header{"Set-Cookie": {"Ding", "Ping"}},
+		Body:              "Hello, World!",
+		IsBase64Encoded:   false,
+	}, nil
+}
+
+func main() {
+	lambda.Start(handle)
 }
